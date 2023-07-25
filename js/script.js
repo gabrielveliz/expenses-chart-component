b50d0aabb81ee3154ddd8acd45ef0904f26ac8df
Gabriel Veliz <gvelizzuniga@gmail.com>
2023-07-25 18:29:10 -0400

    Reto Completado

---
+++ js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	
+	$( "#graf1" )
+		.on( "mouseover", function() {
+	    document.getElementById("uno").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("uno").style.display= "none";	
+	  } );
+
+	 $( "#graf2" )
+		.on( "mouseover", function() {
+	    document.getElementById("dos").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("dos").style.display= "none";	
+	  } );
+
+	 $( "#graf3" )
+		.on( "mouseover", function() {
+	    document.getElementById("tres").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("tres").style.display= "none";	
+	  } );
+
+	 $( "#graf4" )
+		.on( "mouseover", function() {
+	    document.getElementById("cuatro").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("cuatro").style.display= "none";	
+	  } );
+
+	 $( "#graf5" )
+		.on( "mouseover", function() {
+	    document.getElementById("cinco").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("cinco").style.display= "none";	
+	  } );
+
+	 $( "#graf6" )
+		.on( "mouseover", function() {
+	    document.getElementById("seis").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("seis").style.display= "none";	
+	  } );
+
+	 $( "#graf7" )
+		.on( "mouseover", function() {
+	    document.getElementById("siete").style.display= "block";
+	  } )
+	 	.on( "mouseout", function() {
+	    document.getElementById("siete").style.display= "none";	
+	  } );
+
+});
